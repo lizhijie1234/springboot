18bfd784f76977a72641286ed430e18d94d21670
lizhijie <lijiazhijie@163.com>
2019-03-27 10:35:51 +0800

    asfdas

--- src/main/java/com/how2java/springboot/mapper/CategoryMapper.java
+++ src/main/java/com/how2java/springboot/mapper/CategoryMapper.java
@@ -29,3 +29,46 @@ public interface CategoryMapper {
     public int update(Category category);  
 
 }
+
+@Select("select * from category_ ")
+    List<Category> findAll();
+    
+    @Insert(" insert into category_ ( name ) values (#{name}) ") 
+    public int save(Category category);  
+    
+    @Delete(" delete from category_ where id= #{id} ") 
+    public void delete(int id); 
+        
+    @Select("select * from category_ where id= #{id} ") 
+    public Category get(int id); 
+      
+    @Update("update category_ set name=#{name} where id=#{id} ") 
+    public int update(Category category);  
+    @Select("select * from category_ ")
+    List<Category> findAll();
+    
+    @Insert(" insert into category_ ( name ) values (#{name}) ") 
+    public int save(Category category);  
+    
+    @Delete(" delete from category_ where id= #{id} ") 
+    public void delete(int id); 
+        
+    @Select("select * from category_ where id= #{id} ") 
+    public Category get(int id); 
+      
+    @Update("update category_ set name=#{name} where id=#{id} ") 
+    public int update(Category category);  
+    @Select("select * from category_ ")
+    List<Category> findAll();
+    
+    @Insert(" insert into category_ ( name ) values (#{name}) ") 
+    public int save(Category category);  
+    
+    @Delete(" delete from category_ where id= #{id} ") 
+    public void delete(int id); 
+        
+    @Select("select * from category_ where id= #{id} ") 
+    public Category get(int id); 
+      
+    @Update("update category_ set name=#{name} where id=#{id} ") 
+    public int update(Category category);  
